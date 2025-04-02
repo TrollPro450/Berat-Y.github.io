ac8068c01526339a3d515dce0e09fa7a7ec4827a
TrollPro450 <95633888+TrollPro450@users.noreply.github.com>
2025-04-02 12:53:15 +0300

    Update etkileşim.js

--- etkileşim.js
+++ etkileşim.js
@@ -1,35 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const root = document.documentElement;
-    const themeSwitch = document.querySelector(".theme-switch__checkbox");
-
-    // Kaydedilen tema varsa onu yükle
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme === "dark") {
-        root.classList.add("dark-mode");
-        themeSwitch.checked = true;
-    }
-
-    // Butona tıklanınca temayı değiştir
-    themeSwitch.addEventListener("change", function () {
-        if (themeSwitch.checked) {
-            root.classList.add("dark-mode");
-            localStorage.setItem("theme", "dark");
-        } else {
-            root.classList.remove("dark-mode");
-            localStorage.setItem("theme", "light");
-        }
-    });
-});
-
-
-document.querySelector('.theme-switch__checkbox').addEventListener('change', function() {
-    if (this.checked) {
-        document.documentElement.style.setProperty('--ultragrey', '#000000'); // Siyah yap
-    } else {
-        document.documentElement.style.setProperty('--ultragrey', '#f4f4f7'); // Beyaz yap
-    }
-});
-
+// Foto Sistemi
 let currentIndex = 0;
 
 function moveSlide(step) {
